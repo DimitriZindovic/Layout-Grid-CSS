9afef212a06c5d82706197f46cac961afcc84d35
Dimitri Zindovic <114248188+DimitriZindovic@users.noreply.github.com>
2023-12-13 16:29:41 +0100

    🔨 UPDATE readme

--- README.mk
+++ README.mk
@@ -1,3 +1,18 @@
+<h1>Projet CSS layouting</h1>
+
+<h3>Rendu de cours CSS laouting/h3>
+
+<h2>Description :</h2>
+
+Création d'un layout en grid avec une nav, un main avec des posts et un aside
+
+<h2>Technologies :</h2>
+
+HTML
+CSS
+
+<h2>Vidéo expliactive du projet :</h2>
+
 Vidéo n° 1 :
 
 https://youtu.be/A54Qi9x23iM
@@ -5,3 +20,11 @@ https://youtu.be/A54Qi9x23iM
 Vidéo n° 2 :
 
 https://youtu.be/s2QIKnQ68gk
+
+<h2>Auteur :</h2>
+
+Dimitri Zindovic
+
+
+
+
